a473e692acdc2c0446a2a34d2bac61ad2fa218c3
Yannick Wurm <y.wurm@qmul.ac.uk>
2013-09-20 08:49:33 +0100

    first commit

---
+++ checkBridget.rb
@@ -0,0 +1,37 @@
+require 'nokogiri'
+require 'open-uri'
+require 'mail'
+
+url              = 'http://www.triyoga.co.uk/classes/'
+classToSearchFor = 'Anusara Yoga Level 2'
+
+
+options = { :address              => "smtp.gmail.com",
+            :port                 => 587,
+            :user_name            => 'bridgetchecker@gmail.com',
+            :password             => 'isbridgetteaching',
+            :authentication       => 'plain',
+            :enable_starttls_auto => true  }
+
+
+page         = Nokogiri::HTML(open(url))
+scheduleData = page.css('.schedule-data')
+entries      = scheduleData.xpath('//tr')
+
+teacher = nil
+entries.each do |entry|
+  next unless entry.to_s.match(Regexp.new(classToSearchFor))
+  teacher = entry.css(".teacher_panel").css('.content_wrapper').xpath('h3')
+  break
+end
+
+message = "The next '#{classToSearchFor}' class at Triyoga will be given by #{teacher.text}"
+mail = Mail.new do
+  delivery_method :smtp, options
+  to              'tiapio@gmail.com,yannickwurm@gmail.com,bridgetchecker@gmail.com'
+  from            'bridgetchecker@gmail.com'
+  subject         'Next Yoga Class at Triyoga Primrose Hill'
+  body            message
+end
+
+mail.deliver!
